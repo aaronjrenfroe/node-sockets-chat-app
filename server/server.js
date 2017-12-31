f8775ec56f7e10d28b09078c7e7f722def83cd9b
Aaron <afterrabble@gmail.com>
2017-12-30 22:57:49 -0800

    Set up Sockets

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
+// Global Imports
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
+// Local Imports
+// nil
+
+// Constants
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath, {}))
+io.on('connection', (socket) => {
+  console.log('new user connected: ');
+  
+  socket.on('disconnect', () => {
+    console.log('Client Disconnected from Server')
+  })
+});
 
-
-
-
-
-app.listen(PORT, ()=> {
+// Lastestes
+server.listen(PORT, ()=> {
   console.log('Serving on port: ' + PORT);
 });
